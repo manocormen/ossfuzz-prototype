c56f7280ff96d3e1a0b71ad7501ca6b080dd19a1
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2025-03-24 16:57:11 +0100

    Add function for fetching details of a single project
    It returns a selection of fields; tailor as needed (TODO).

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 """OSS-Fuzz-Research"""
 
+import base64
+
 import httpx
+import yaml
 from dotenv import load_dotenv
 
 load_dotenv()
 
 
 PROJECTS_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects"
+PROJECT_YAML_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects/{project_name}/project.yaml"
 
 
-def get_projects():
+def get_all_projects():
     response = httpx.get(PROJECTS_ENDPOINT)
     projects = [
         {
@@ -19,3 +23,19 @@ def get_projects():
         for project in response.json()
     ]
     return projects
+
+
+def get_project_details(project_name: str):
+    url = PROJECT_YAML_ENDPOINT.format(project_name=project_name)
+    response = httpx.get(url)
+    project = yaml.safe_load(base64.b64decode(response.json()["content"]))
+    metadata = {
+        "name": project_name,
+        "language": project.get("language"),
+        "homepage": project.get("homepage"),
+        "main_repo": project.get("main_repo"),
+        "primary_contact": project.get("primary_contact"),
+        "vendor_ccs": project.get("vendor_ccs", []),
+        "fuzzing_engines": project.get("fuzzing_engines", []),
+    }
+    return metadata
