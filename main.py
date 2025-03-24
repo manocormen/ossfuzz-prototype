5b571e44abe121552667e585ed8fe5ec83a89795
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2025-03-24 18:11:08 +0100

    Extend project details function to also include build system
    OSS-Fuzz allows a variety of build systems. We can try to identify
    which system a project uses by looking for keywords in build.sh.
    To do this accurately for all projects, we'd need an exhaustive
    list of allowed build systems and more refined logic.
    
    As a starting point, I'm limiting myself to the build systems
    mentioned by David and Adam in the article below. I imagine they
    used those as example because they're common cases:
    
    https://adalogics.com/blog/fuzzing-100-open-source-projects-with-oss-fuzz
    
    One way to improve this functionality would be to read through the
    build.sh documentation specific to each language (TODO) - e.g.:
    
    https://google.github.io/oss-fuzz/getting-started/new-project-guide/python-lang/#buildsh

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ load_dotenv()
 
 PROJECTS_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects"
 PROJECT_YAML_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects/{project_name}/project.yaml"
+BUILD_SH_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects/{project_name}/build.sh"
 
 
 def get_all_projects():
@@ -25,10 +26,29 @@ def get_all_projects():
     return projects
 
 
+def infer_build_system(file: str):
+    file = file.lower()
+    if "cmake" in file:
+        return "cmake"
+    if "make" in file:
+        return "make"
+    if "ninja" in file:
+        return "ninja"
+    if "bazel" in file:
+        return "bazel"
+    return None
+
+
 def get_project_details(project_name: str):
     url = PROJECT_YAML_ENDPOINT.format(project_name=project_name)
     response = httpx.get(url)
     project = yaml.safe_load(base64.b64decode(response.json()["content"]))
+
+    url = BUILD_SH_ENDPOINT.format(project_name=project_name)
+    response = httpx.get(url)
+    build_file = base64.b64decode(response.json()["content"]).decode()
+    build_system = infer_build_system(build_file)
+
     metadata = {
         "name": project_name,
         "language": project.get("language"),
@@ -37,5 +57,6 @@ def get_project_details(project_name: str):
         "primary_contact": project.get("primary_contact"),
         "vendor_ccs": project.get("vendor_ccs", []),
         "fuzzing_engines": project.get("fuzzing_engines", []),
+        "build_system": build_system,
     }
     return metadata
