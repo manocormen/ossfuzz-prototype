7fe05264cbf8fdbdba80e78d8689d2ab7a3b9a04
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2025-03-25 19:34:38 +0100

    Add function for bulk-fetching and caching key project files
    In preparation for the filtering functionality, I added simple
    caching of key files (project.yaml and build.sh) for all projects.
    
    There are currently over 1000 projects. GitHub's REST API limits
    queries to 60 per hour when unauthenticated and 5000 when
    authenticated. Fetching 2 files for each project requires over
    2000 queries: impossible for unauthenticated users, and borderline
    for authenticated if they refresh cache. We need room for growth.
    
    Instead, I decided to use GitHub's GraphQL API, which is designed
    for big queries. It does require simple authentication (so does
    the REST API to get reasonable limits anyways), but it solves
    entirely the requests limit problem. It also has a limit of 5000
    requests per hour, but unlike the REST API, each GraphQL request
    can retrieve many files. In fact, retrieving the over 2000 files
    we need need requires just 2 requests (we could do just 1, but 2
    makes for more readable code).
    
    For convenience, the caching is currently done with a global. I'll
    correct this down the road (TODO).

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """OSS-Fuzz-Research"""
 
 import base64
+import os
+from collections import defaultdict
 
 import httpx
 import yaml
@@ -12,6 +14,11 @@ load_dotenv()
 PROJECTS_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects"
 PROJECT_YAML_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects/{project_name}/project.yaml"
 BUILD_SH_ENDPOINT = "https://api.github.com/repos/google/oss-fuzz/contents/projects/{project_name}/build.sh"
+GITHUB_GRAPHQL_ENDPOINT = "https://api.github.com/graphql"
+
+_projects_cache = {}
+
+BATCH_SIZE = 100
 
 
 def get_all_projects() -> list[dict[str, str]]:
@@ -27,7 +34,7 @@ def get_all_projects() -> list[dict[str, str]]:
     return projects
 
 
-def infer_build_system(file: str) -> str | None:
+def _infer_build_system(file: str) -> str | None:
     """Heuristically infer the build system using in a file."""
     file = file.lower()
     if "cmake" in file:
@@ -50,7 +57,7 @@ def get_project_details(project_name: str) -> dict[str, str | list[str]]:
     url = BUILD_SH_ENDPOINT.format(project_name=project_name)
     response = httpx.get(url)
     build_file = base64.b64decode(response.json()["content"]).decode()
-    build_system = infer_build_system(build_file)
+    build_system = _infer_build_system(build_file)
 
     metadata = {
         "name": project_name,
@@ -63,3 +70,71 @@ def get_project_details(project_name: str) -> dict[str, str | list[str]]:
         "build_system": build_system,
     }
     return metadata
+
+
+def _sanitize_identifier(identifier: str) -> str:
+    """Replace invalid characters in identifier with underscores."""
+    return identifier.replace(".", "_").replace("-", "_")
+
+
+def _fetch_project_files_query(project_names: list[str], filename: str) -> str:
+    """Return query for fetching project files in bulk via GitHub's GraphQL API."""
+    inner_fragment = "\n".join(
+        [
+            f"""
+                {_sanitize_identifier(pname)}: object(expression: "HEAD:projects/{pname}/{filename}") {{
+                    ... on Blob {{
+                        {_sanitize_identifier(filename)}: text
+                    }}
+                }}
+            """
+            for pname in project_names
+        ]
+    )
+    query = f"""
+        {{
+            repository(owner: "google", name: "oss-fuzz") {{
+                {inner_fragment}
+            }}
+        }}
+    """
+    return query
+
+def _merge_by_project(projects1, projects2):
+    """Merge two dictionaries of projects into one, keyed by project names."""
+    merged_projects = defaultdict(dict)
+    for pname in projects1:
+        merged_projects[pname].update(projects1[pname] or {})
+        merged_projects[pname].update(projects2[pname] or {})
+    return dict(merged_projects)
+
+
+def _fetch_project_files(
+    project_names: list[str], filename: str
+) -> dict[str, dict[str, str]]:
+    """Fetch specific file for given OSS-Fuzz projects."""
+    headers = {"Authorization": f"Bearer {os.getenv('GITHUB_TOKEN')}"}
+    payload = {"query": _fetch_project_files_query(project_names, filename)}
+    response = httpx.post(
+        GITHUB_GRAPHQL_ENDPOINT, headers=headers, json=payload, timeout=30
+    )
+    projects = response.json()["data"]["repository"]
+    return projects
+
+
+def _batch_list(list_: list, batch_size: int):
+    """Break input list into batches."""
+    for i in range(0, len(list_), batch_size):
+        yield list_[i : i + batch_size]
+
+
+def cache_all_projects() -> None:
+    """Fetch and cache select data for all OSS-Fuzz projects (in batches)."""
+    project_names = [project["name"] for project in get_all_projects()]
+    project_yaml_files, build_sh_files = {}, {}
+    print("Please wait: caching project data... (just once)")
+    for pnames in _batch_list(project_names, BATCH_SIZE):
+        project_yaml_files.update(_fetch_project_files(pnames, "project.yaml"))
+        build_sh_files.update(_fetch_project_files(pnames, "build.sh"))
+    global _projects_cache
+    _projects_cache = _merge_by_project(project_yaml_files, build_sh_files)
