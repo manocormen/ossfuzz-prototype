bbb85c22d09354b2acba865cd79a5565a48db94e
Manoel Cortes Mendez <manoelcortesmendez@gmail.com>
2025-03-29 13:47:47 +0100

    Add functions for filtering projects using predicates or keywords
    I've implemented two ways for users to filter projects: the first
    involves returning projects that satisfy a user-defined predicate
    (highly flexible but slightly complex); the second involves
    matching keywords (simple but more limited).
    
    Later, if useful, we may embrace a full-on specification pattern,
    to support fine-grained filtering (AND, OR, etc.) without users
    needing to write custom predicates.

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
 """Interact with API"""
 
+from collections.abc import Callable
+
 from dotenv import load_dotenv
 
 from cache import Cache
@@ -29,11 +31,43 @@ def get_project(project_name: str) -> Project:
     return project
 
 
-def get_projects(limit: int | None = None, clear_cache: bool = False) -> Projects:
+def get_projects(limit: int | None = None) -> Projects:
     """Return the details of all projects."""
-    if not _cache or clear_cache:
+    if not _cache:
         cache_projects(limit)
     return _cache.projects
+
+
+def filter_projects(predicate: Callable, limit: int | None = None) -> Projects:
+    """Return the details of all projects that satisfy the predicate."""
+    if not _cache:
+        cache_projects(limit)
+    return {name: proj for name, proj in _cache.projects.items() if predicate(proj)}
+
+
+def match_projects(
+    name: str | None = None,
+    language: str | None = None,
+    homepage: str | None = None,
+    main_repo: str | None = None,
+    primary_contact: str | None = None,
+    vendor_ccs: list[str] | None = None,
+    fuzzing_engines: list[str] | None = None,
+    build_system: str | None = None,
+    limit: int | None = None,
+) -> Projects:
+    """Return the details of all the projects that match all the given keywords."""
+    keywords = {k: v for k, v in locals().items() if (v is not None and k != "limit")}
+
+    def is_match(project: Project) -> bool:
+        """Return true if the given project matches all the keywords in the closure."""
+        for attr_name, keyword in keywords.items():
+            attr_value = getattr(project, attr_name)
+            if attr_value and keyword not in attr_value:
+                return False  # If a single keyword is missing, no match, return early
+        return True
+
+    return filter_projects(is_match, limit)
 
 
 def cache_projects(limit: int | None = None) -> None:
